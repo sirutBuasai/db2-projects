724cfe7655119ddeedbd6612db8649ebf602d3cc
Sirut Buasai <sirutbuasai27@hotmail.com>
2022-03-26 19:07:58 -0400

    getters and setter

--- dbms-buffer/src/Frame.java
+++ dbms-buffer/src/Frame.java
@@ -4,7 +4,7 @@ public class Frame {
   private boolean pinned;
   private int block_id;
 
-  // Constructor
+  // Constructor ------------------------------------------
   public Frame(char[] content, boolean dirty, boolean pinned, int block_id) {
     // content size handling
     if (content.length > 4096) {
@@ -19,6 +19,47 @@ public class Frame {
     }
   }
 
+  // Getters ----------------------------------------------
+  public char[] getContent() {
+    return this.content;
+  }
+
+  public boolean getDirty() {
+    return this.dirty;
+  }
+
+  public boolean getPinned() {
+    return this.pinned;
+  }
+
+  public int getBlockId() {
+    return this.block_id;
+  }
+
+  // Setters ----------------------------------------------
+  public void setContent(char[] content) {
+    // content size handling
+    if (content.length > 4096) {
+      throw new java.lang.Error("Error: content length exceeds 4KB!");
+    }
+    else {
+      this.content = content;
+    }
+  }
+
+  public void setDirty(boolean dirty) {
+    this.dirty = dirty;
+  }
+
+  public void setPinned(boolean pinned) {
+    this.pinned = pinned;
+  }
+
+  public void setBlockId(int block_id) {
+    this.block_id = block_id;
+  }
+
+  // Other methods ----------------------------------------
   public void print_content() {
     for (int i = 0; i < this.content.length; i++) {
       System.out.println(this.content[i]);
