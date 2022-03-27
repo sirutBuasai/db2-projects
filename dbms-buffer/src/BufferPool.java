4654456d573ca4dd8bd8fae50516064adf822a20
Sirut Buasai <sirutbuasai27@hotmail.com>
2022-03-26 23:50:57 -0400

    move test file

--- dbms-buffer/src/BufferPool.java
+++ dbms-buffer/src/BufferPool.java
@@ -3,10 +3,18 @@ public class BufferPool {
   Frame[] buffers;
 
   // Constructor ------------------------------------------
-  public BufferPool(int buffer_size) {
-    this.buffers = new Frame[buffer_size];
+  public BufferPool() {
+    this.buffers = new Frame[1];
   }
 
   // Other methods ----------------------------------------
+  public void initialize(int buffer_size) {
+    this.buffers = new Frame[buffer_size];
 
+    for (int i = 0; i < buffer_size; i++) {
+      Frame f = new Frame(i+1);
+      f.initialize();
+      this.buffers[i] = f;
+    }
+  }
 }
