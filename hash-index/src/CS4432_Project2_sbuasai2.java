87ab898700a84ebd2f246fd042dd8b804a3fa977
Sirut Buasai <sirutbuasai27@hotmail.com>
2022-04-18 12:39:18 -0400

    rename main file

--- hash-index/src/CS4432_Project2_sbuasai2.java
+++ hash-index/src/CS4432_Project2_sbuasai2.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main {
+public class CS4342_Project2_sbuasai2 {
 
     /*
     Since input is limited to just a few commands, I have basically hardcoded them.
