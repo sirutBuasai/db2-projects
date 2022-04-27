d9b3d6f821131973e73b5529877467913322304e
Sirut Buasai <sirutbuasai27@hotmail.com>
2022-04-26 21:39:33 -0400

    problem 1

---
+++ query-handler/Database.java
@@ -0,0 +1,154 @@
+import java.util.*;
+import java.io.IOException;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class Database {
+  // Global variables
+  private int NUMBUCKET = 50;
+  private int NUMRECORD = 100;
+  private int NUMFILE = 99;
+  private int RECORDSIZE = 40;
+
+  // Class attributes
+  private ArrayList<LinkedList<String>> hash_table;
+  private String[] file_data;
+
+  /*
+   * Constructor ------------------------------------------
+   */
+  public Database() {
+    this.hash_table = new ArrayList<LinkedList<String>>(Collections.nCopies(NUMBUCKET, new LinkedList<String>()));
+    this.file_data = new String[NUMFILE*NUMRECORD];
+  }
+
+  /*
+   * ------------------------------------------------------
+   * Hash function to convert randomV to bucket number
+   * Argument: int randomV
+   * Return: int bucket_num
+   */
+  private int hashFunc(int randomV) {
+    // restrict the bucket_num value using mod
+    int bucket_num = randomV % NUMBUCKET;
+    return bucket_num;
+  }
+
+  /*
+   * ------------------------------------------------------
+   * Given a key:value pair, put the content into the hash table
+   * Argument: int key, String value
+   * Return: void
+   */
+  private void hashPut(int key, String value) {
+    // pass randomV into the hash function to get the bucket number
+    int bucket_num = hashFunc(key);
+    // put the key:value pair into the hash table
+    this.hash_table.get(bucket_num).add(value);
+  }
+
+  /*
+   * ------------------------------------------------------
+   * Get a record content given a file and the record number
+   * Argument: String file_content, int r_num
+   * Return: String record_content
+   */
+  private String getRecord(String file_content, int r_num) {
+    // given that r_num is already 0-indexed, we can calculate for the starting and ending index of the record
+    int start_idx = RECORDSIZE * r_num;
+    int end_idx = start_idx + RECORDSIZE;
+    // retrieve record content using substring
+    String record_content = file_content.substring(start_idx, end_idx);
+    return record_content;
+  }
+
+  /*
+   * ------------------------------------------------------
+   * Get a record randomV given a record content
+   * Argument: String record_content
+   * Return: String randomV
+   */
+  private int getRandomV(String record) {
+    // randomV is always at position record_content[33:37]
+    int randomV = Integer.parseInt(record.substring(33, 37));
+    return randomV;
+  }
+
+  /*
+   * ------------------------------------------------------
+   * Construct a hash table for relation A
+   * Argument: void
+   * Return: void
+   */
+  public void buildTable() {
+    // reinitialize the hast table
+    this.hash_table = new ArrayList<LinkedList<String>>(Collections.nCopies(NUMBUCKET, new LinkedList<String>()));
+    try {
+      // loop over all files in dataset
+      for (int i = 0; i < NUMFILE; i++) {
+        // initialize file name
+        String file_name = "Project3Dataset-A/A" + (i+1) + ".txt";
+        // read file content
+        Scanner scanner = new Scanner(new File(file_name));
+        String file_content = scanner.nextLine();
+        scanner.close();
+        // loop over all records in the current file
+        for (int j = 0; j < NUMRECORD; j++) {
+          // retrieve record content and its randomV
+          String curr_record = getRecord(file_content, j);
+          int curr_randomV = getRandomV(curr_record);
+          // add value to hash table
+          hashPut(curr_randomV, curr_record);
+        }
+      }
+    } catch (IOException e) {
+      System.err.println("Error: Cannot open file.");
+    }
+  }
+
+  /*
+   * ------------------------------------------------------
+   * Join relation A and B together on column randomV
+   * Argument: void
+   * Return: void
+   */
+  public void joinTable() {
+    // print column fields
+    int someint = 0;
+    System.out.println("A.Col1   A.Col2      B.Col1   B.Col2");
+    try {
+      // loop over files, same thing as constructArray
+      for (int i = 0; i < NUMFILE; i++) {
+        // initialize file name
+        String file_name = "Project3Dataset-B/B" + (i+1) + ".txt";
+        // read file content
+        Scanner scanner = new Scanner(new File(file_name));
+        String file_content = scanner.nextLine();
+        scanner.close();
+        // loop over all records in the current file
+        for (int j = 0; j < NUMRECORD; j++) {
+          // retrieve record content and its randomV
+          String record_b = getRecord(file_content, j);
+          int randomV_b = getRandomV(record_b);
+          // get bucket_num from randomV
+          int search_bucket = hashFunc(randomV_b);
+          // loop over records in tableA hash table
+          for (String record_a : this.hash_table.get(search_bucket)) {
+            int randomV_a = getRandomV(record_a);
+            if (randomV_a == randomV_b) {
+              someint++;
+              String[] colsA = record_a.split(", ");
+              String[] colsB = record_b.split(", ");
+              // print output
+              System.out.println(colsA[0] + "   " + colsA[1] + "      " + colsB[0] + "   " + colsB[1]);
+            }
+          }
+
+        }
+      }
+    } catch (FileNotFoundException e) {
+      System.out.println("File not found!");
+    }
+    System.out.println(someint);
+  }
+}
--- query-handler/Main.java
+++ query-handler/Main.java
@@ -5,31 +5,34 @@ import java.util.concurrent.TimeUnit;
 
 public class Main {
   public static void main(String args[]) throws IOException {
-    // initialize forever loop for the program
+    // initialize variables required for the system
     BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
     boolean active = true;
+    Database db = new Database();
     System.out.println("Program initiated, waiting for command...");
 
+    // initialize forever loop for the program
     while (active) {
       System.out.print(">> ");
       // initialize scanner class for input handling
       // clean up input for better argument handling
-      String input = reader.readLine().toUpperCase().replace(" ", "");
+      String input = reader.readLine().toUpperCase();
       // initialize timer
       long start = System.nanoTime();
 
       // switch case statements for handling different commands
       switch (input) {
-        case "SELECTA.COL1,A.COL2,B.COL1,B.COL2FROMA,BWHEREA.RANDOMV=B.RANDOMV":
-          System.out.println("1");
+        case "SELECT A.COL1, A.COL2, B.COL1, B.COL2 FROM A, B WHERE A.RANDOMV = B.RANDOMV":
+          db.buildTable();
+          db.joinTable();
           // construct hash table
           // constructHashTable();
           // join tables
           // joinTables();
           break;
 
-        case "SELECTCOUNT(*)FROMA,BWHEREA.RANDOMV>B.RANDOMV":
-          System.out.println("1");
+        case "SELECT COUNT(*) FROM A, B WHERE A.RANDOMV > B.RANDOMV":
+          System.out.println("2");
           // construct array
           // constructArray();
           // select count
@@ -74,18 +77,19 @@ public class Main {
 
         case "EXIT":
           active = false;
+          break;
 
         case "HELP":
-          System.err.println("List of commands:");
-          System.err.println("                SELECT A.COL1, A.COL2, B.COL1, B.COL2 FROM A, B WHERE A.RANDOMV = B.RANDOMV");
-          System.err.println("                SELECT COUNT(*) FROM A, B WHERE A.RANDOMV > B.RANDOMV");
-          System.err.println("                EXIT");
+          System.out.println("List of commands:");
+          System.out.println("                SELECT A.COL1, A.COL2, B.COL1, B.COL2 FROM A, B WHERE A.RANDOMV = B.RANDOMV");
+          System.out.println("                SELECT COUNT(*) FROM A, B WHERE A.RANDOMV > B.RANDOMV");
+          System.out.println("                EXIT");
           System.out.println("-----------------------------------");
-          
+          break;
 
         default:
           System.err.println("Error: Invalid input.");
-          System.err.println("Please check command input for typos.");
+          System.err.println("Commands are space-sensitive, please type in commands exactly as shown below.");
           System.err.println("List of commands:");
           System.err.println("                SELECT A.COL1, A.COL2, B.COL1, B.COL2 FROM A, B WHERE A.RANDOMV = B.RANDOMV");
           System.err.println("                SELECT COUNT(*) FROM A, B WHERE A.RANDOMV > B.RANDOMV");
